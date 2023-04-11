81af2285d894e180d4065bdafcd21918c22f3888
tedijs123 <122751472+tedijs123@users.noreply.github.com>
2023-04-11 10:06:21 +0300

    pogas

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const plusins = document.querySelector('.js-plus');
-const inputins = document.querySelector('.js-input');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answer = document.querySelector('.js-result');
 
 const Add = (cipars1, cipars2) => {
     return cipars1 + cipars2
@@ -18,5 +23,7 @@ const Divide = (cipars1, cipars2) => {
 }
 
 plusins.addEventListener('click', () => {
-    
+    answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
+
+
 })
